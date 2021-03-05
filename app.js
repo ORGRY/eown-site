765bf821f6d522a8c6e1bfe8b617117d2619d27c
葛飞 <f.ge@htd.cn>
2021-03-05 17:10:45 +0800

    update

--- app.js
+++ app.js
@@ -18,13 +18,41 @@ rp.get({
       date: article.created_at
     })
     // read template html
+    var result = '';
+    fs.readFile('./templates/post.html', 'utf8', function (err, data) {
+      if (err) { console.log(err); return false; }
+      result = data.replace(/articleDate/g, new Date(article.created_at).Format('yyyy-MM-dd'));
+      result = result.replace(/articleContent/g, article.body_html);
+    });
     fs.readFile('./templates/default.html', 'utf8', function (err, data) {
       if (err) { console.log(err); return false; }
-      console.log(data)
-      var result = data.replace(/articleTitle/g, article.title);
-      result = result.replace(/articleContent/g, article.body_html);
-      fs.writeFile(`./articles/${article.id}.html`, result, () => console.log(article.id))
+      var allHtml = data.replace(/articleTitle/g, article.title);
+      allHtml = allHtml.replace(/articleHtml/g, result);
+      fs.writeFile(`./articles/${article.id}.html`, allHtml, () => console.log(article.id))
     });
   }
   fs.writeFile('./articles/list.json', JSON.stringify(list), () => console.log('list.json'))
-})
+});
+
+Date.prototype.Format = function (fmt) {
+  var o = {
+    'M+': this.getMonth() + 1,
+    'd+': this.getDate(),
+    'H+': this.getHours(),
+    'm+': this.getMinutes(),
+    's+': this.getSeconds(),
+    'S+': this.getMilliseconds()
+  };
+  //因为date.getFullYear()出来的结果是number类型的,所以为了让结果变成字符串型，下面有两种方法：
+  if (/(y+)/.test(fmt)) {
+    //第一种：利用字符串连接符“+”给date.getFullYear()+''，加一个空字符串便可以将number类型转换成字符串。
+    fmt = fmt.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
+  }
+  for (var k in o) {
+    if (new RegExp('(' + k + ')').test(fmt)) {
+      //第二种：使用String()类型进行强制数据类型转换String(date.getFullYear())，这种更容易理解。
+      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (('00' + o[k]).substr(String(o[k]).length)));
+    }
+  }
+  return fmt;
+};
